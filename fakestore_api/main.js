77bae4c7b9bace062dddb16ed7edac522963de1a
Xaviermora <xaviermoravega12@gmail.com>
2022-05-23 21:40:07 -0300

    practica con la api fakestore

--- fakestore_api/main.js
+++ fakestore_api/main.js
@@ -7,13 +7,13 @@ const productos = async (categoria) => {
 
     datos_productos.forEach(producto => {
         const card_producto = `
-            <div class="col-lg-4 col-sm-6 col-12 mb-3">
-                <div class="card h-100">
-                    <img src="${producto.image}" class="card-img-top my-2 mx-auto" alt="Imagen del producto" style="height: 20rem; width: 75%">
+            <div class="col-lg-4 col-md-6 col-10 mb-4 mx-md-0 mx-auto">
+                <div class="card h-100 shadow">
+                    <img src="${producto.image}" class="card-img-top my-2 mx-auto" alt="Imagen del producto" style="height: 17rem; width: 50%">
                     <hr>
                     <div class="card-body d-flex flex-column">
                         <h5 class="card-title">${producto.title}</h5>
-                        <p class="card-text fw-bolder">$${producto.price}</p>
+                        <p class="card-text">$${producto.price}<span class="float-end me-4">${producto.rating.rate}/5</span></p>
                         <a href="#" class="btn btn-primary mt-auto col-lg-6 col-md-8">Comprar</a>
                     </div>
                 </div>
@@ -25,3 +25,5 @@ const productos = async (categoria) => {
     document.getElementById('titulo').innerHTML = datos_productos[0].category
     document.getElementById('productos').innerHTML = lista_productos.join('')
 }
+
+productos()
